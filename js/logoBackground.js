21edcf41de634997eb22585191af747a3f8a5106
Annieday <adai1@ualberta.ca>
2015-12-07 10:43:34 -0700

    fix some layout bug for logo. delete green/blue from logo background lines.

--- js/logoBackground.js
+++ js/logoBackground.js
@@ -1,5 +1,5 @@
 /*
- * http://codepen.io/jackrugile/pen/bdwvMo 
+ * FROM http://codepen.io/jackrugile/pen/bdwvMo 
  * 
  * */
 var canvas,
@@ -19,7 +19,14 @@ function line() {
     this.target = {x: width / 2, y: height / 2};
     this.dist = 0;
     this.angle = 0;
-    this.hue = tick / 5;
+    this.hue = tick / 5 % 360;
+    console.log(this.hue);
+    if (this.hue <= 80 && this.hue + 30 >= 80) {
+        this.hue = randInt(0, 50);
+    } else if (this.hue >= 80 && this.hue <= 220) {
+        this.hue = randInt(220, 360);
+    }
+    console.log(this.hue);
     this.life = 1;
     this.updateAngle();
     this.updateDist();
